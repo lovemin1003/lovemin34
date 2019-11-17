2fd8e054cef0560baeb0006bd33c5480fd430444
lovemin34 <52541535+lovemin34@users.noreply.github.com>
2019-11-17 10:41:51 +0900

    1
    1

--- 컬랙션 프레임웍/src/propreties3.java
+++ 컬랙션 프레임웍/src/propreties3.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -32,3 +33,39 @@ public class propreties3 {
 	}//main의 끝
 
 }
+=======
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class propreties3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		Properties 담원 = new Properties();
+		
+		
+		담원.setProperty("너구리", "탑");
+		담원.setProperty("펀치", "정글");
+		담원.setProperty("쇼메이커", "미드");
+		담원.setProperty("뉴클리어", "원딜");
+		담원.setProperty("beryl", "support");
+		
+		try
+		{
+		
+			담원.store(new FileOutputStream("담원.txt"), "담원 게이밍,damwon gaming");//한글은 깨져보임.
+			
+			담원.storeToXML(new FileOutputStream("담원.xml"), "담원 게이밍");
+		
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}//main의 끝
+
+}
+>>>>>>> parent of f46b05c... Revert "자바 공부한 것-2(javaで勉強した事　その2） "
--- 컬랙션 프레임웍/src/vectors.java
+++ 컬랙션 프레임웍/src/vectors.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Vector;
 
 public class vectors {
@@ -35,3 +36,42 @@ public class vectors {
 	}
 
 }
+=======
+import java.util.Vector;
+
+public class vectors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vector v = new Vector(5);//용량(capacity)
+		
+		v.add(1);
+		v.add(2);
+		v.add(3);
+		print(v);
+		
+		v.trimToSize();//빈 공간을 제거함.
+		print(v);
+		
+		v.ensureCapacity(10);//용량 설정
+		print(v);
+		
+		v.setSize(5);//사이즈 설정
+		print(v);
+		
+		v.clear();//사이즈 제거
+		print(v);
+
+	}
+
+	private static void print(Vector v) {
+		// TODO Auto-generated method stub
+		System.out.println(v);
+		System.out.println(v.size());
+		System.out.println(v.capacity());//용량(capacity)
+		System.out.println();
+	}
+
+}
+>>>>>>> parent of f46b05c... Revert "자바 공부한 것-2(javaで勉強した事　その2） "
